cb007e19e3c5e85b7bf5d7e338a800cd1b49aa84
GQue - DEVELOPER FRONT END <68132577+fcurrao@users.noreply.github.com>
2023-01-30 14:12:18 -0300

    first commit

---
+++ js/archivo.js
@@ -0,0 +1,122 @@
+ /////  boton mas
+ const logo = document.getElementById('divpromo1');
+ const girando = () => {
+    // logo.style.
+ }
+
+
+///////////////////////////////////////////////////////////
+//  boton mas
+///////////////////////////////////////////////////////////
+
+// const contenedorPlus = document.getElementById('divpromo1');
+// var statusContenedor = false;
+
+
+
+// const ejecutandose = () => {
+//     if (statusContenedor == false) {
+
+//         contenedorPlus.innerHTML = `
+//         <div class="divpromox">
+//         <div class="df">
+//             <img class="imgmbps" src="../public_html/img/50.png" alt="mbps" width="90px" height="65px">
+//             <p class="textpromos">+ 80 Canales + 50 Canales HD</p>
+//         </div>
+//         <div>
+//             <buton class="boton1fijo cccc">LO QUIERO</buton>
+            
+
+
+
+//         </div>
+//     </div>
+    
+
+//         ` 
+        
+//     } if (statusContenedor == true) {
+
+//         contenedorPlus.innerHTML = `
+// <div id="divpromo2" class="divpromo2">
+// <div class="divpromox2">
+//     <div class="df22">
+//         <img class="imgmbps" src="../public_html/img/50.png" alt="mbps" width="90px" height="65px">
+//         <p class="textpromos">+ 80 Canales + 50 Canales HD</p>
+//     </div>
+
+
+
+//     <buton class="boton1fijo botonplus">LO QUIERO</buton>
+
+//     <div>
+//         <div id="seveono" class="parteoculta">
+//             <div class="dispflex">
+//                 <p>TELEFONÍA FIJA: Llamadas ilimitadas MEX / EUA / CAN / Incluye llamadas ilimitadas a
+//                     celular</p><br></br>
+
+
+//                 <div class="other">
+//                     <img clas="imgcc" src="../public_html/img/wifi (1).svg"></img> &nbsp;&nbsp;Conexión a
+//                     Internet
+
+
+//                     <div class="other2"> <img clas="imgcc" src="../public_html/img/ui.svg"></img>
+//                     </div>
+//                     <div class="other3">
+//                         <p> Llamadas ilimitadas </p>
+//                         <p class="tt">Nacionales | A EUA y Canadá | A móviles </p>
+//                     </div>
+//                 </div>
+
+//             </div>
+
+
+//         </div>
+        
+//     </div>
+
+// </div>
+
+// </div>
+ 
+
+
+// `  
+
+//     }
+// }
+
+
+
+// const botonprueba = document.getElementById('divconteplus');
+// botonprueba.addEventListener('click', () => {
+//     if(statusContenedor == false){
+//         botonprueba.innerHTML =  `<div id="divless" class="botonmas plusss less"><p id="botonmasplus" class="menoss"> -</p></div>`
+//         statusContenedor = true;
+//     }else {
+//         statusContenedor = false;
+//         botonprueba.innerHTML =  `<div id="divplus" class="botonmas plusss"><p id="botonmasplus" class="plus">+</p></div>`
+//     }
+    
+//     console.log("statusContenedor", statusContenedor)
+//     ejecutandose()
+
+//     })
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
